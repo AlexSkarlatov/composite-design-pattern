7007d2834fba38a42da2a80ba9dc3a792daf31e9
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-09-02 14:31:06 -0400

    this is more hierarchical

--- src/com/skarlatov/Manager.java
+++ src/com/skarlatov/Manager.java
@@ -7,8 +7,14 @@ package com.skarlatov;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Directory implements Employee {
+public class Manager implements Employee {
+    private String name;
+    private double salary;
 
+    public Manager(String name, double salary){
+        this.name = name;
+        this.salary = salary;
+    }
 
 
     private List<Employee> employeeList = new ArrayList<>();
@@ -18,9 +24,22 @@ public class Directory implements Employee {
 //        TODO add member field called String description
         //some data about this directory type refactor lator
         //system.out.println(description);
+        System.out.println("-------------------");
+        System.out.println("Name = " + getName());
+        System.out.println("salary =" + getSalary());
+        System.out.println("---------------------");
         for(Employee e: employeeList){
             e.showEmployeeDetails();
         }
+    }
+    public double getSalary(){
+        return this.salary;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public Employee getChild(int i ) {
+        return employeeList.get(i);
     }
     public void addEmployee(Employee emp){
         employeeList.add(emp);
